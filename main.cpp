60fcc2878d0c6fcd701f22c751a61b416f734963
AERTY  PINTO DOS SANTOS <>
2021-04-16 01:58:19 +0000

    parte 2

--- main.cpp
+++ main.cpp
@@ -1,21 +1,68 @@
 #include <iostream>
+
 using namespace std;
+
+bool confere(string p){
+
+  if(p.length() != 4){
+    return false;
+  }
+  
+      if(p[0] != '#'){
+      return false;
+    }
+
+    if(p[1] <= '0'){
+      return false;
+    }
+
+    if(p[2] != '='){
+      return false;
+    }
+
+    if(p[3] <= '0'){
+      return false;
+    }
+
+  
+    return true;
+  
+}
 
 int main() {
 
   int N = 0;
   string palavra =""; 
+  bool op = true;
 
-  cout << "Informe um numero N que identificara quantas teclas serao informadas\n";
-  cin >> N;
-  
+  while(op){
 
-  for(int i = 0;i<N;i++){
-     cout << "informe o caractere sharp (#),";
-     cout << " o numero da tecla,";
-     cout << "caractere igual (=) e o numero de vezes que a teclar sera pressionada.\n";
-     cin >> palavra;
+    cout << "Informe um numero N que identificara quantas teclas serao informadas\n";
+    cin >> N;
+
+    if(N <=0){
+       cout << "Valor invalido,Informe um valor valido!\n";
+    }else{
+      op = false;
+    }
   }
   
+  
 
+  while(N > 0){
+
+     cout << "\ninforme o caractere sharp (#),";
+     cout << " o numero da tecla,";
+     cout << "caractere igual (=) e o numero de vezes que a teclar sera pressionada\n";
+     cin >> palavra;
+
+     if(confere(palavra)){
+       N--;
+       decodificar(palavra);
+     }else{
+       cout << "Valor invalido,Informe um valor valido!\n";
+     }
+
+  }
+    
 }
