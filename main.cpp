52b85e06bcf207729f931541dded17961e276e47
AERTY  PINTO DOS SANTOS <>
2021-04-16 00:59:25 +0000

    inicio

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+
+  int N = 0;
+  string palavra =""; 
+
+  cout << "Informe um numero N que identificara quantas teclas serao informadas\n";
+  cin >> N;
+  
+
+  for(int i = 0;i<N;i++){
+     cout << "informe o caractere sharp (#),";
+     cout << " o numero da tecla,";
+     cout << "caractere igual (=) e o numero de vezes que a teclar sera pressionada.\n";
+     cin >> palavra;
+  }
+  
+
 }
