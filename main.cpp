202b257159ce7df4da7c57745b4ed5354fe12dd8
AERTY  PINTO DOS SANTOS <>
2021-04-16 02:35:42 +0000

    final

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+string guardaPalavra;
+
 bool confere(string p){
 
   if(p.length() != 4){
@@ -12,7 +14,7 @@ bool confere(string p){
       return false;
     }
 
-    if(p[1] <= '0'){
+    if(p[1] <= '1'){
       return false;
     }
 
@@ -24,9 +26,116 @@ bool confere(string p){
       return false;
     }
 
-  
     return true;
+}
+
+char decodificar(string p){
   
+    if(p[1] == '2'){
+      if(p[3] == '1')
+      return 'a';
+
+      if(p[3] == '2')
+      return 'b';
+
+      if(p[3] == '3')
+      return 'c';
+      
+    }
+
+    if(p[1] == '3'){
+
+      if(p[3] == '1')
+      return 'd';
+
+      if(p[3] == '2')
+      return 'e';
+
+      if(p[3] == '3')
+      return 'f';
+    }
+
+    if(p[1] == '4'){
+
+      if(p[3] == '1')
+      return 'g';
+
+      if(p[3] == '2')
+      return 'h';
+
+      if(p[3] == '3')
+      return 'i';
+    }
+
+    if(p[1] == '5'){
+
+      if(p[3] == '1')
+      return 'j';
+
+      if(p[3] == '2')
+      return 'k';
+
+      if(p[3] == '3')
+      return 'l';
+    }
+
+    if(p[1] == '6'){
+
+      if(p[3] == '1')
+      return 'm';
+
+      if(p[3] == '2')
+      return 'n';
+
+      if(p[3] == '3')
+      return 'o';
+      
+    }
+
+    if(p[1] == '7'){
+
+      if(p[3] == '1')
+      return 'p';
+
+      if(p[3] == '2')
+      return 'q';
+
+      if(p[3] == '3')
+      return 'r';
+
+      if(p[3] == '4')
+      return 's';
+    }
+
+    if(p[1] == '8'){
+      
+      if(p[3] == '1')
+      return 't';
+
+      if(p[3] == '2')
+      return 'u';
+
+      if(p[3] == '3')
+      return 'v';
+    }
+
+    if(p[1] <= '9'){
+
+      if(p[3] == '1')
+      return 'w';
+
+      if(p[3] == '2')
+      return 'x';
+
+      if(p[3] == '3')
+      return 'y';
+
+      if(p[3] == '4')
+      return 'z';
+    }
+
+    return '0';
+    
 }
 
 int main() {
@@ -42,6 +151,8 @@ int main() {
 
     if(N <=0){
        cout << "Valor invalido,Informe um valor valido!\n";
+    }else if(N > 50){
+       cout << "Valor invalido,Informe um valor menor ou igual a 50!\n";
     }else{
       op = false;
     }
@@ -58,11 +169,13 @@ int main() {
 
      if(confere(palavra)){
        N--;
-       decodificar(palavra);
+       guardaPalavra += decodificar(palavra);
      }else{
        cout << "Valor invalido,Informe um valor valido!\n";
      }
 
   }
+
+   cout << "Resultado:\n"<<guardaPalavra;
     
 }
